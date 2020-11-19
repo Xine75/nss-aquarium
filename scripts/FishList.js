a35d6cf25bc86a7a035e7232f6b51aa1a7824182
Xine75 <cjdoza@gmail.com>
2020-11-19 13:30:40 -0600

    added holy fish etc

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -4,21 +4,37 @@
 
 
 
-import { useFish } from "./FishDataProvider.js"
+import { mostHolyFish } from "./FishDataProvider.js"
+import { soldierFish } from "./FishDataProvider.js"
+import { nonHolyFish } from "./FishDataProvider.js"
+
 import { Fish } from "./Fish.js"
 
 export const FishList = () => {
 
     // Get a reference to the `<article class="content">` element
     const contentElement = document.querySelector(".container--left")
-    const allTheFish = useFish()
 
-      // Generate all of the HTML for all of the fish
-      let fishHTMLRepresentations = ""
-      for (const fishObject of allTheFish) {
-          console.log("one fish!", fishObject)
+    const holyFish = mostHolyFish();
+    const soldiers = soldierFish();
+    const losers = nonHolyFish();
+
+
+      for (const fishObject of holyFish) {
+          // console.log("one fish!", fishObject)
           const fishHTMLRepresentations = Fish(fishObject)
           
         contentElement.innerHTML += fishHTMLRepresentations
+  }
+  for (const fishObject of soldiers) {
+    // console.log("one fish!", fishObject)
+    const fishHTMLRepresentations = Fish(fishObject)
+    contentElement.innerHTML += fishHTMLRepresentations
+
+}  for (const fishObject of losers) {
+  // console.log("one fish!", fishObject)
+  const fishHTMLRepresentations = Fish(fishObject)
+  contentElement.innerHTML += fishHTMLRepresentations
 }
 }
+
